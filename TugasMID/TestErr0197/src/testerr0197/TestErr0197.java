8af79d41b4e379a9065430085a0bc248c4fc670c
nurhidaya197 <128206705+nurhidaya197@users.noreply.github.com>
2023-03-30 10:25:09 +0700

    Add files via upload

---
+++ TugasMID/TestErr0197/src/testerr0197/TestErr0197.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testerr0197;
+
+/**
+ * Nama:Nurhidayah Mansyur
+ * Stambuk:13020210197
+ */
+import java.util.Scanner;
+public class TestErr0197 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner masukan=new Scanner(System.in);
+        int i = 10; 
+        double k = 100;
+        char c = "s";
+        double else = true;
+        int j = i+1;
+        System.out.println("j adalah "+ j + "k adalah "+k + " dan c adalah "+c + "dan trues adalah "+false);
+    }
+    
+}
---
+++ TugasMID/TestErr20197/src/testerr20197/TestErr20197.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testerr20197;
+
+/**
+ * Nama:Nurhidayah Mnasyur
+ * Stambuk:13020210197
+ */
+import java.util.Scanner;
+public class TestErr20197 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner masukan=new Scanner(System.in);
+        for int i=0;
+        i<10;  i++;
+        if =+ i;
+        if(i<j);
+        system.out.println(j);
+        system.out.println(j);
+        do{
+             j++;
+        }while j<10;
+         while (j<10);{
+        j++;
+        }
+    }
+    
+}
